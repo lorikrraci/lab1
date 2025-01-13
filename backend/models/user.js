2f9da40da10f7e42dbcdb482ab48f1b8dbcea5c5
albzhubi <albzhubi@gmail.com>
2025-01-14 00:14:40 +0100

    deleteuser

--- backend/models/user.js
+++ backend/models/user.js
@@ -54,4 +54,17 @@ module.exports = {
         const [result] = await db.execute(sql, values);
         return result.affectedRows; // number of updated rows
     },
+    // DELETE user
+    deleteUser: async (id) => {
+        const sql = 'DELETE FROM users WHERE id = ?';
+        const [result] = await db.execute(sql, [id]);
+        return result.affectedRows; // number of rows deleted
+    },
+
+    // Check if an email is already taken excluding a specific user ID
+    getUserByEmailExcludingId: async (email, excludeId) => {
+        const sql = `SELECT * FROM users WHERE email = ? AND id <> ?`;
+        const [rows] = await db.execute(sql, [email, excludeId]);
+        return rows[0];
+    },
 }
