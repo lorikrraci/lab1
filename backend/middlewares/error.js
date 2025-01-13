20254fe1e2da0dc0d74008f5f49796100a9958ca
albzhubi <albzhubi@gmail.com>
2025-01-14 00:16:32 +0100

    error.js start

---
+++ backend/middlewares/error.js
@@ -0,0 +1,15 @@
+const ErrorHandler = require('../utils/errorHandler');
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+
+    if (process.env.NODE_ENV === 'DEVELOPMENT') {
+        
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+            errMesage: err.message,
+            stack: err.stack
+        });
+    }
+}
