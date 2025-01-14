c33cbd8b69c5385efaed20d6b1bd9835180cf35e
Lorik <lorikrraci00@gmail.com>
2025-01-14 15:06:21 +0100

    app update

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const app = express();
+
+// Environment Variables
+require('dotenv').config();
+
+// Import all the routes
+const products = require('./routes/product');
 
 // Route Handling
 app.use('/api/v1/products', products);
 
+// Middleware to handle errors
+app.use(errorMiddleware);
  
 module.exports = app;
