cd0751146fee62cd73ce9b4489b9a78192f5cd07
Art Pozhegu <ap64927@ubt-uni.net>
2025-01-14 17:51:09 +0100

    update

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -8,4 +8,16 @@ const {
     updateOrderStatus, 
     deleteOrder
 } = require('../controllers/orderController');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+
+router.route('/order/new').post(isAuthenticatedUser, newOrder);
+
+router.route('/order/me').get(isAuthenticatedUser, getOrders); // Adjust if it fetches orders by user
+router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
+
+router.route('/admin/order').get(isAuthenticatedUser, authorizeRoles('admin'), getOrders); // Adjust if needed
+router.route('/admin/order/:id')
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateOrderStatus)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteOrder);
+module.exports = router;
 
