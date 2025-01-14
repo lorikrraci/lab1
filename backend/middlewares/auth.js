efc3fef35da489f07433bcf9645b5db885df6286
Arb Sahatqija <as67822@ubt-uni.net>
2025-01-14 13:43:17 +0100

    Authentication of users

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("./catchAsyncErrors");
+const User = require('../models/user'); // Correctly pointing to the consolidated user model
+
+// Checks if user is authenticated or not
+exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
+    const { token } = req.cookies;
+
+    if (!token) {
+        return next(new ErrorHandler('Login first to access this resource.', 401));
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.getUserById(decoded.id);
+
+        if (!user) {
+            return next(new ErrorHandler('User not found.', 404));
+        }
+
+        req.user = user; // Attach user to request
+        next();
+    } catch (error) {
+        return next(new ErrorHandler('Invalid Token. Please login again.', 401));
+    }
+});
