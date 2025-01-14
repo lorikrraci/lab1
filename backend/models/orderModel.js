a435de74630bb651a269acc96f81568e25b1ce7c
Art Pozhegu <ap64927@ubt-uni.net>
2025-01-14 18:23:31 +0100

    orders for a user

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,4 +1,12 @@
 const db = require('../config/database'); // a mysql2 Pool or Connection
 
+module.exports = {
+    // 1. Get all orders for a specific user
+    getOrdersByUserId: async (userId) => {
+      const sql = 'SELECT * FROM orders WHERE userId = ?';
+      const [rows] = await db.execute(sql, [userId]);
+      return rows; // array of order objects
+    },
+}  
 
   
