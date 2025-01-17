cadb7fc559bb62458138329ce458b47abfe74cd8
LedionKurhasku <ledionkurhasku29@gmail.com>
2025-01-17 18:06:20 +0100

    admin routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -17,3 +17,8 @@ const {
 // Define your routes here
 router.route('/').get(getProducts); 
 router.route('/:id').get(getProductById);
+// Admin Routes
+router.route('/admin/products/new').post(isAuthenticatedUser, authorizeRoles('admin'), createProduct);
+router.route('/admin/products/:id')
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
