d9be7195efe8a19aa9e58d02957ab8e4555a2ffb
Lorik <lorikrraci00@gmail.com>
2025-01-17 17:43:25 +0100

    server

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,14 @@ require('dotenv').config();
 const app = require('./app');
 const db = require('./config/database'); 
 const dotenv = require('dotenv');
+
+// Handle Uncaught Exceptions
+process.on('uncaughtException', err => {
+    console.error(`Uncaught Exception: ${err.message}`);
+    console.error(err.stack);
+    console.log('Shutting down due to uncaught exceptions.');
+    process.exit(1);
+});
+
+// Setting up config file
+dotenv.config({ path: 'backend/config/.env' });
