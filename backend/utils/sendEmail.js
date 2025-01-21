643c505abb2adb53904e6540579cee15d51862f5
Arb Sahatqija <as67822@ubt-uni.net>
2025-01-21 12:50:02 +0100

    Finished Send Email Code

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -10,12 +10,14 @@ const sendEmail = async (options) => {
         },
     });
 
+    const message = {
+        from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+    };
 
-
-
-
-
-
-
-
+    await transporter.sendMail(message);
 };
+
+module.exports = sendEmail;
