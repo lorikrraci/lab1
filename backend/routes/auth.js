a1b3b79dd29394c7deee7daffbd40cae78e47f25
albzhubi <albzhubi@gmail.com>
2025-01-26 18:04:24 +0100

    auth2

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,3 +8,13 @@ const {
     getUserProfile, 
     updateUserProfile 
 } = require('../controllers/authController'); // Ensure only one import for these
+
+const { isAuthenticatedUser } = require('../middlewares/auth'); // Authentication middleware
+
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+router.route('/logout').get(logoutUser);
+router.route('/me').get(isAuthenticatedUser, getUserProfile);
+router.route('/me/update').put(isAuthenticatedUser, updateUserProfile);
+
+module.exports = router;
