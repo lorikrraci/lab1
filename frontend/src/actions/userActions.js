b1147b46f97b1c1ec74afe4ad3a024da1b8aff18
LedionKurhasku <ledionkurhasku29@gmail.com>
2025-01-31 02:01:56 +0100

    login

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -16,3 +16,29 @@ const axiosInstance =  axios.create({
       'Content-Type': 'application/json',  // Optional: Default headers
     },
   });
+  //Login
+export const login = (email, password) => async (dispatch )=>{
+    try{
+
+        dispatch({ type: LOGIN_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.post('http://localhost:5000/api/v1/auth/login', {email, password }, config)
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: data.user
+        })
+
+    }catch(error){
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
