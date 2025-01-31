0bfc02e763e1ae97f0ccec6bc21dd2e84129571e
albzhubi <albzhubi@gmail.com>
2025-01-31 19:55:47 +0100

    Stats.js

---
+++ frontend/src/components/pages/Club.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Club = () => {
+  return (
+    <div>
+      <h2>Klubi</h2>
+      <p>Informacione për klubin tuaj...</p>
+    </div>
+  );
+};
+
+export default Club;
